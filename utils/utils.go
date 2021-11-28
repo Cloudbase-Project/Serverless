c3c18f81523b687a6278f2ec8181e0f09d13d28b
Navaneeth <vnavaneeth15@gmail.com>
2021-11-28 22:12:06 +0530

    create utility function to build full image name

---
+++ utils/utils.go
@@ -0,0 +1,12 @@
+package utils
+
+// returns a fully qualified image name given a function id.
+//
+// eg: quay.io/ubuntu-project/ubuntu:latest
+func BuildImageName(functionId string) string {
+	Registry := "ghcr.io"
+	Project := ""
+
+	imageName := Registry + "/" + Project + "/" + functionId + ":latest"
+	return imageName
+}
